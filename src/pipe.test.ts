449b4a3fd76d8cc5088c9fdfa9ec3a9388952ac3
Mitsuki Watanabe <ix5231@gmail.com>
2024-07-15 16:19:13 +0900

    update test

--- src/pipe.test.ts
+++ src/pipe.test.ts
@@ -1,14 +1,35 @@
-import { describe, test, expect } from 'vitest';
+import { describe, test, expect } from "vitest";
 
-import { Pipe, PipeAsync, PipeSync } from './pipe';
+import { Pipe, PipeAsync, PipeSync } from "./pipe";
 
-describe('Pipe factory', () => {
-    test('Select class by value', () => {
-        expect(Pipe(1)).instanceOf(PipeSync);
-        expect(Pipe(Promise.resolve(1))).instanceOf(PipeAsync);
-    })
+describe("Pipe factory", () => {
+  test("Select class by value", () => {
+    expect(Pipe(1)).instanceOf(PipeSync);
+    expect(Pipe(Promise.resolve(1))).instanceOf(PipeAsync);
+  });
 });
 
-// describe.todo('PipeSync');
+describe("and", () => {
+  test("sync", () => {
+    expect(
+      Pipe(1)
+        .and((n) => n + 1)
+        .and((n) => n.toString()).value
+    ).toEqual("2");
+  });
 
-// describe.todo('PipeAsync');
+  test("async", () => {
+    expect(
+      Pipe(Promise.resolve(1))
+        .and(async (n) => n + 1)
+        .and(async (n) => n.toString()).value
+    ).resolves.toEqual("2");
+  });
+
+  test("merge to async", () => {
+    const initial = Pipe(1);
+    expect(initial).instanceOf(PipeSync);
+    const turnToAsync = Pipe(1).and(async (n) => n + 1);
+    expect(turnToAsync).instanceOf(PipeAsync);
+  });
+});
